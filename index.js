5129aa368f704ccd37767120814d137699f93549
chiptus <chiptus@gmail.com>
2017-04-26 16:54:18 +0300

    move IIFE

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-(function() {
-  'use strict';
-  var libPrefix = process.env.COVER ? './lib-cov' : './lib';
+var libPrefix = process.env.COVER ? './lib-cov' : './lib';
 
-  module.exports = process.browser
-    ? {
-        compare: require('./lib/compare'),
-        XMLSerializer: require('./lib/canonizer'),
-        revXPath: require('./lib/revxpath'),
-        GroupingReporter: require('./lib/reporters/groupingReporter.js'),
-      }
-    : {
-        compare: require(libPrefix + '/compare'),
-        XMLSerializer: require(libPrefix + '/canonizer'),
-        revXPath: require(libPrefix + '/revxpath'),
-        GroupingReporter: require(libPrefix + '/reporters/groupingReporter.js'),
-      };
-})();
+module.exports = process.browser
+  ? {
+    compare: require('./lib/compare'),
+    XMLSerializer: require('./lib/canonizer'),
+    revXPath: require('./lib/revxpath'),
+    GroupingReporter: require('./lib/reporters/groupingReporter.js'),
+  }
+  : {
+    compare: require(libPrefix + '/compare'),
+    XMLSerializer: require(libPrefix + '/canonizer'),
+    revXPath: require(libPrefix + '/revxpath'),
+    GroupingReporter: require(libPrefix + '/reporters/groupingReporter.js'),
+  };
