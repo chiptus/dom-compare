c7eefa1c21f2974869cf7ccbaf0b36b46fd09f7f
chiptus <chiptus@gmail.com>
2017-04-30 16:53:45 +0300

    export diff and node types

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ module.exports = process.browser
     XMLSerializer: require('./lib/canonizer'),
     revXPath: require('./lib/revxpath'),
     GroupingReporter: require('./lib/reporters/groupingReporter.js'),
+    DIFF_TYPES: require('./lib/diff_types'),
+    NODE_TYPES: require('./lib/node_types'),
   }
   : {
     compare: require(libPrefix + '/compare'),
     XMLSerializer: require(libPrefix + '/canonizer'),
     revXPath: require(libPrefix + '/revxpath'),
     GroupingReporter: require(libPrefix + '/reporters/groupingReporter.js'),
+    DIFF_TYPES: require('./lib/diff_types'),
+    NODE_TYPES: require('./lib/node_types'),
   };
